4c798d808e48a88022e0b9a891afbbaa503abed7
ephigenia <ephigenia@4856e329-c559-0410-8ab9-924015c2118c>
2009-02-10 13:06:39 +0000

    + added hint were to change 404 error message to 404 error message if in developer mode
    git-svn-id: file:///Users/ephigenia/Sites/ephFrame/repository@148 4856e329-c559-0410-8ab9-924015c2118c

--- 0.2/ephFrame/view/error/404.php
+++ 0.2/ephFrame/view/error/404.php
@@ -2,3 +2,9 @@
 <p>
 	The page you requested could not be found. Please check the url and try again.
 </p>
+<?php if (class_exists('Registry') && Registry::get('DEBUG') > DEBUG_PRODUCTION) { ?>
+<p class="hint">
+	You can customize this error message by creating your own 404.php file in:<br />
+	<q><?= VIEW_DIR ?>error/404.php</q>
+</p>
+<?php } ?>
