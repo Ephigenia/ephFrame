cabb3b9a0644288909d67f373dc6e1f8ab7c7a11
ephigenia <ephigenia@4856e329-c559-0410-8ab9-924015c2118c>
2009-04-05 12:45:14 +0000

    + jquery simplePreview url button fixed
    git-svn-id: file:///Users/ephigenia/Sites/ephFrame/repository@177 4856e329-c559-0410-8ab9-924015c2118c

--- 0.2/app/html/static/js/jquery.plugin.simplePreview.js
+++ 0.2/app/html/static/js/jquery.plugin.simplePreview.js
@@ -14,31 +14,35 @@
  */
 (function($) {
 
-	$.fn.simplePreview = function(configure) {
-		$(this).loadCSS(document.WEBROOT + 'static/css/simplePreview.css');
+	$.fn.simplePreview = function(options) {
+		
 		$(this).loadJS(document.WEBROOT + 'static/js/jquery.plugin.dialog.js');
-		$(this).loadJS(document.WEBROOT + 'static/js/jquery.plugin.dialog.js');
-		var inputName = $(this).attr('name');
-		var previewName = inputName + 'preview';
-		// insert preview pange
-		$(this).after(
-			'<div id="' + previewName + '" class="simplePreview">'+($(this).html())+'</div>'
-		);
-		// insert toolbar buttons
-		var toolbarButtons = {
+		
+		// default simplePreview configuration
+		var defaults = {
+			showPreview: false,
+			buttons: ['bold', 'italic', 'quote', 'url', 'image' ]
+		};
+		// default button configuration
+		var possibleButtons = {
 			bold: { label: 'bold', replaceSelection: '<strong>%s</strong>' },
 			italic: { label: 'italic', replaceSelection: '<em>%s</em>' },
 			quote: { label: 'quote', replaceSelection: '<q>%s</q>' },
-			code: { label: 'code', replaceSelection: '<code>%s</code>' },
+			//code: { label: 'code', replaceSelection: '<code>%s</code>' },
+			
+			// url replacement
 			url: { label: 'url', callback: function(config, selection) {
-				$('body').dialog.prompt('Add URL', 'Please enter a valid URL here:', 'http://www.', function(url) {
-					if (url == '' || url == 'http://www.') return;
-					if (selection.text == '') selection.text = url;
-					config.target.replaceSelection('<a href="' + url + '">' + selection.text + '</a>');
-					
-				});
-				config.target.trigger('keyup');
-			}},
+					$('body').dialog.prompt('Add URL', 'Please enter a valid URL here:', 'http://www.', function(url) {
+						if (url == '' || url == 'http://www.') return;
+						if (selection.text == '') selection.text = url;
+						var text = config.target.val();
+						config.target.val(text.substr(0, selection.start) + '<a href="' + url + '">' + selection.text + '</a>' + text.substr(selection.end));
+					});
+					config.target.trigger('keyup');
+				} // function
+			},
+			
+			// image insertion
 			image: { label: 'image', callback: function(config, selection) {
 				$('body').dialog.prompt('Add Image', 'Please enter a valid URL to an image here:', 'http://www.', function(src) {
 					if (src == '' || src == 'http://www.') return;
@@ -55,39 +59,69 @@
 				});
 			}}
 		}
-		var toolbarClassName = inputName + 'Toolbar';
-		$(this).before('<div class="' + toolbarClassName + ' simplePreviewToolbar" />');
-		var toolbar = $('.' + toolbarClassName);
-		for (i in toolbarButtons) {
-			if (typeof(toolbarButtons[i]) !== 'object') continue;
-			var btnConfig = toolbarButtons[i];
-			btnConfig.target = $(this);
-			var btnClassName = inputName + 'Button' + btnConfig.label;
-			toolbar.append('<a href="javascript:void(0);" title="' + btnConfig.label + '" class="' + btnClassName + ' ' + btnConfig.label + '"><span>' + btnConfig.label + '</span></a>');
-			// add click action to buttons
-			$('.' + btnClassName).data('btnConfig', btnConfig);
-			$('.' + btnClassName).click(function() {
-				var btnConfig = $(this).data('btnConfig');
-				var selection = btnConfig.target.getSelection();
-				if (btnConfig.replaceSelection) {
-					var replaced = btnConfig.replaceSelection.replace(/%s/, selection.text);
-					btnConfig.target.replaceSelection(replaced);
-				} else if (btnConfig.callback) {
-					btnConfig.callback(btnConfig, selection);
-				}
-				btnConfig.target.trigger('keyup');
-			});
+		
+		var config = $.extend(defaults, options);
+		
+		var usedButtons = {};
+		if (typeof(config.buttons) == 'string') {
+			config.buttons = config.buttons.split(',')
 		}
-		// key listener that updates preview
-		$(this).keyup(function() {
-			if ($(this).val() !== '') {
-				$('#' + previewName).html($(this).val());
-				$('#' + previewName).show();
-			} else {
-				$('#' + previewName).hide();
+		for (i in config.buttons) {
+			var buttonName = config.buttons[i];
+			if (typeof(possibleButtons[buttonName]) == 'object') {
+				usedButtons[buttonName] = possibleButtons[buttonName];
 			}
-		});
-		$(this).trigger('keyup');
+		}
+		
+		var inputName = $(this).attr('name');
+		
+		// TOOLBAR
+		// --------
+		if (typeof(usedButtons) == 'object') {
+			var toolbarClassName = inputName + 'Toolbar';
+			$(this).before('<div class="' + toolbarClassName + ' simplePreviewToolbar" />');
+			var toolbar = $('.' + toolbarClassName);
+			for (i in usedButtons) {
+				var btnConfig = usedButtons[i];
+				if (typeof(btnConfig) !== 'object') continue;
+				btnConfig.target = $(this);
+				var btnClassName = inputName + 'Button' + btnConfig.label;
+				toolbar.append('<a href="javascript:void(0);" title="' + btnConfig.label + '" class="' + btnClassName + ' ' + btnConfig.label + '"><span>' + btnConfig.label + '</span></a>');
+				// add click action to buttons
+				$('.' + btnClassName).data('btnConfig', btnConfig);
+				$('.' + btnClassName).click(function() {
+					var btnConfig = $(this).data('btnConfig');
+					var selection = btnConfig.target.getSelection();
+					if (btnConfig.replaceSelection) {
+						var replaced = btnConfig.replaceSelection.replace(/%s/, selection.text);
+						btnConfig.target.replaceSelection(replaced);
+					} else if (btnConfig.callback) {
+						btnConfig.callback(btnConfig, selection);
+					}
+					btnConfig.target.trigger('keyup');
+				});
+			}
+		}
+		
+		// PREVIEW-PANE
+		// ------------	
+		if (config.showPreview) {
+			var previewName = inputName + 'preview';
+			$(this).after('<div id="' + previewName + '" class="simplePreview">' + ($(this).html()) + '</div>');
+			// key listener that updates preview
+			$(this).keyup(function(){
+				if ($(this).val() !== '') {
+					// replace line breaks in preview
+					var text = $(this).val().replace(/\n/g, '<br />');
+					$('#' + previewName).html(text);
+					$('#' + previewName).show();
+				} else {
+					$('#' + previewName).hide();
+				}
+			});
+			$(this).trigger('keyup');
+		}
+		
 	};
 	
 })(jQuery);
