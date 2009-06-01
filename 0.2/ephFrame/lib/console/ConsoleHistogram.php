1182551d8b6314101e77ef9d4421bb8f0caef124
ephigenia <ephigenia@4856e329-c559-0410-8ab9-924015c2118c>
2009-06-01 20:56:31 +0000

    git-svn-id: file:///Users/ephigenia/Sites/ephFrame/repository@206 4856e329-c559-0410-8ab9-924015c2118c

--- 0.2/ephFrame/lib/console/ConsoleHistogram.php
+++ 0.2/ephFrame/lib/console/ConsoleHistogram.php
@@ -4,14 +4,20 @@ class_exists('ConsoleDrawing') or require dirname(__FILE__).'/../ConsoleDrawing.
 
 /**
  * 	ephFrame: <http://code.moresleep.net/project/ephFrame/>
- * 	Copyright 2007+, Ephigenia M. Eichner, Kopernikusstr. 8, 10245 Berlin
+ * 	Copyright (c) 2007+, Ephigenia M. Eichner
+ * 						 Kopernikusstr. 8
+ * 						 10245 Berlin
  *
  * 	Licensed under The MIT License
  * 	Redistributions of files must retain the above copyright notice.
- * 	@license http://www.opensource.org/licenses/mit-license.php The MIT License
- * 	@copyright Copyright 2007+, Ephigenia M. Eichner
- * 	@link http://code.ephigenia.de/projects/ephFrame/
- * 	@filesource
+ * 
+ * 	@license		http://www.opensource.org/licenses/mit-license.php The MIT License
+ * 	@copyright		copyright 2007+, Ephigenia M. Eichner
+ * 	@link			http://code.ephigenia.de/projects/ephFrame/
+ * 	@version		$Revision$
+ * 	@modifiedby		$LastChangedBy$
+ * 	@lastmodified	$Date$
+ * 	@filesource		$HeadURL$
  */
 
 /**
