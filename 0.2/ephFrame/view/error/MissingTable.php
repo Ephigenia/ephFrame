7d4431b75e7c3ceb088132b38e07a3a7829d916b
moresleep.net <moresleep.net@4856e329-c559-0410-8ab9-924015c2118c>
2009-06-01 21:03:01 +0000

    nothing
    git-svn-id: file:///Users/ephigenia/Sites/ephFrame/repository@207 4856e329-c559-0410-8ab9-924015c2118c

--- 0.2/ephFrame/view/error/MissingTable.php
+++
@@ -1,9 +0,0 @@
-<h1>Missing Database Table <q><?= $tablename ?></q></h1>
-<p class="error">
-	A model is missing a table named <q><?= $tablename ?></q> in the database.
-	Please create that table or change the models tablename to match.
-</p>
-<p class="hint">
-	You can edit this error message by creating your own view for it in
-	<q><?= VIEW_DIR ?>error/<?= basename(__FILE__) ?></q>
-</p>
