310c88e9cda6c245715b98112c23eeb932230df8
Ephigenia <love@ephigenia.de>
2010-10-12 14:52:51 +0200

    -

---
+++ app/lib/component/form/AppForm.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * ephFrame: <http://code.marceleichner.de/project/ephFrame/>
+ * Copyright (c) 2007+, Ephigenia M. Eichner
+ *                      Brunnenstr. 10
+ *                      10119 Berlin
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ * @license		http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @copyright	copyright 2007+, Ephigenia M. Eichner
+ * @link		http://code.marceleichner.de/projects/ephFrame/
+ * @filesource
+ */
+
+/**
+ * Mother Class for all Application Forms
+ * 
+ * Use this class to add methods to you application wide used forms.
+ * See the cookbook how to use this in your applications.
+ * 
+ * @author Marcel Eichner // Ephigenia <love@ephigenia.de>
+ * @since 23.10.2008
+ * @package app
+ * @subpackage app.lib.component
+ */
+class AppForm extends Form
+{}
