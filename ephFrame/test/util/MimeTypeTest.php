30ee57e7d870d8e3461195a7fa80d6538fbbe943
Ephigenia <love@ephigenia.de>
2011-03-09 19:21:03 +0100

    * added tests for mimetype class

---
+++ ephFrame/test/util/MimeTypeTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ephFrame\test\util;
+
+use ephFrame\util\MimeType;
+
+class MimeTypeTest extends \PHPUnit_Framework_TestCase
+{
+	public function testGetByFilenameValues()
+	{
+		return array(
+			array('myfilename.csv','text/comma-separated-values'),
+			array('multiple.ext.txt', 'text/plain'),
+			array('txt', 'text/plain'),
+			array('.txt', 'text/plain'),
+			array('.', false),
+			array('', false),
+			array('nothing', false),
+		);
+	}
+	
+	/**
+	 * @dataProvider testGetByFilenameValues
+	 */
+	public function testGetByFilename($filename, $result)
+	{
+		$this->assertEquals(MimeType::get($filename), $result);
+	}
+	
+	public function testGetByExtensionValues()
+	{
+		return array(
+			array('txt', 'text/plain'),
+			array('text', false),
+		);
+	}
+	
+	/**
+	 * @dataProvider testGetByExtensionValues
+	 */
+	public function testGetByExtension($extension, $result)
+	{
+		$this->assertEquals(MimeType::get($extension), $result);
+	}
+}
