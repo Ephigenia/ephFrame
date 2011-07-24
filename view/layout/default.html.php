046bbb130957b5b6a0e2cc1980bba257a66ec27f
Ephigenia <love@ephigenia.de>
2011-07-24 18:45:48 +0200

    reduced content of the public directory

--- view/layout/default.html.php
+++ view/layout/default.html.php
@@ -4,7 +4,25 @@
 	<meta charset="utf-8">
 	<title><?php echo @$pageTitle ?: '[no title]' ?></title>
 	<base href="<?php echo \ephFrame\core\Router::base(); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo ephFrame\core\Router::base(); ?>/css/app.css">
+	<style type="text/css" media="screen">
+		body {
+			font: normal 18px/28px Baskerville, Garamond, "Lucida Sans Unicode",Arial,Verdana,sans-serif;
+			color: #4C4C4C;
+			padding: 1em;
+			background-color: #E6E6E6;
+		}
+
+		a, a:visited {
+			color: #0080FF;
+		}
+
+		h1 {
+			text-shadow: 0px 1px 1px rgba(0,0,0,0.25), 0px -1px 1px rgba(255,255,255,0.25);
+			color: #000;
+			font-weight: bold;
+			font-size: 3em;
+		}
+	</style>
 </head>
 <body>
 	<div id="app">
