53289016e2f8da73703549a74d68a5582130685e
Ephigenia <love@ephigenia.de>
2011-08-19 10:15:01 +0200

    * added switch for html wrapper decorator to add element class optional

--- lib/ephFrame/HTML/Form/Decorator/HTMLTag.php
+++ lib/ephFrame/HTML/Form/Decorator/HTMLTag.php
@@ -9,15 +9,24 @@ class HTMLTag extends Decorator
 	public $tag = 'div';
 	
 	public $value = false;
+	
+	public $addElementClass = true;
 
 	public $attributes = array(
 		'escaped' => false,
+		'class' => array(), 
 	);
 	
 	public $position = Position::WRAP;
-	
+
 	public function __toString()
 	{
+		if ($this->addElementClass) {
+			$ElementClass = substr(strrchr(get_class($this->element), '\\'), 1);
+			if (!in_array($ElementClass, $this->attributes)) {
+				$this->attributes['class'] = $ElementClass;
+			}
+		}
 		if (empty($this->value)) {
 			return '';
 		}
